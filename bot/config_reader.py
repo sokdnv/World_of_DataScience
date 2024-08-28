fa11543d3ba96b665f1f509b392e118f22d1ed78
kudinov <s.kudinov@list.ru>
2024-08-28 18:40:34 +0300

    config reader update

--- bot/config_reader.py
+++ bot/config_reader.py
@@ -5,6 +5,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     BOT_TOKEN: SecretStr
     CHAT_API: SecretStr
+    DB_KEY: SecretStr
 
     class Config:
         env_file = ".env"
